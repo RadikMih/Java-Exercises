5ed9632e5c5994a72709f6d9a692c545ef115082
RadikMih <radik.mihnev@gmail.com>
2018-12-20 00:11:54 +0200

    solved task

---
+++ src/dsa/BubbleSort.java
@@ -0,0 +1,37 @@
+package dsa;
+
+// space O(1)
+// time O(n^2)
+
+public class BubbleSort {
+    private static void bubbleSort(int[] array){
+        boolean isSorted = false;
+        int lastUnsorted = array.length - 1;
+
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < lastUnsorted; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                    isSorted = false;
+                }
+            }
+            lastUnsorted--;
+        }
+    }
+
+    private static void swap(int[] array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {9, 7, 4, 3, 2, 1, 8, 5};
+        bubbleSort(array);
+
+        for (int number : array) {
+            System.out.print(number + " ");
+        }
+    }
+}
